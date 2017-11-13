c483f92e7bc79a4ab3ff39c8872245c6b6b6877d
mbeck16@mail.csuchico.edu <mbeck16@ecc-jaguar.csuchico.edu>
2017-11-13 11:41:12 -0800

    Fully functioning program

--- main.cpp
+++ main.cpp
@@ -1,19 +1,18 @@
-#include<iostream>
-#include<string>
-#include"bitmap.h"
-#include<vector>
-#include<cstdlib>
+#include <iostream>
+#include <string>
+#include "bitmap.h"
+#include <vector>
+#include <cstdlib>
 
 using namespace std;
 
 bool goodImage(string);
 void OverlayImages();
 
-bool done = false;
-
 int MaxImages = 10;
 
 int imageCount;
+
 string imgName;
 
 Bitmap image;
@@ -21,78 +20,70 @@ Pixel rgb;
 
 vector < vector <vector <Pixel> > > bmp;
 
-//Make the user input the image names, checking if each one is in a proper format
-//load the ones that are legitimate, have the same size as the first image and are .bmp into an array of vectors saved from the bitmap
-
-//OverlayImage
-    //Make a nested for loop like you would with an array but have another one in the center to get the image you want from the index
-    //Get image[index][row][col]
-    //get each pixel rgb value and divide it by the number of images
-    //set the pixel in the first image as that
-    //ask the user what's the name they want to save it as
-
-//Print out every quarter of the rows are started
-
-//functions
-    //check image for errors
-        //if the image ends in .bmp
-        //if the length of the row and column are equal to the first image
-        //return true
-        //report an error for wrong size or type
-
 int main()
 {
+    bmp.resize(1);
     for(int i = 0; i < MaxImages;)
     {
-        cout << "Enter the name of the file you would like to add.\n";
+        cout << "Enter the name of the file you would like to add." << endl;
         cin >> imgName;
-
-        image.open(imgName);
         
-        if(imgName[imgName.length() - 4] == '.' && imgName[imgName.length() - 3] == 'b' && imgName[imgName.length() - 2] == 'm' && imgName[imgName.length() - 1] == 'p'/* &&
-         image.isImage() != 0*/)
-         {
-            if(i > 0)
+        if(imgName == "Done" || imgName == "done" || imgName == "DONE")
+        {
+            if(i <= 1)
             {
-                bmp[i] = image.toPixelMatrix();
-                i++;
-                
+                cout << "You need to enter two or more images" << endl;
+                return 0;
             }
-            else if(bmp[0].size() == bmp[i].size() && bmp[0][0].size() == bmp[i][0].size())
-            {
-                bmp[i] = image.toPixelMatrix();
-                i++;
-                
-            }
-            else
-            {
-                cout << "Wrong dimensions.";
-            }
-         }
-         else if(imgName == "DONE" || imgName == "Done" || imgName == "done")
-         {
             i = MaxImages;
-         }
-         else
-         {
-            cout << "File is not valid.\n";
-         }
+        }
+        else
+        {
+            image.open(imgName);
+        }
+       
+        
+            
+            if(imgName[imgName.length() - 4] == '.' && imgName[imgName.length() - 3] == 'b' && imgName[imgName.length() - 2] == 'm' && imgName[imgName.length() - 1] == 'p' && image.isImage() && i != MaxImages)
+           {
+        
+                if(i == 0)
+                {
+        
+                    bmp[i] = image.toPixelMatrix();
+                    i++;
+                    bmp.resize(i + 1);
+                }
+                else
+                {
+                        
+                    bmp[i] = image.toPixelMatrix();
+        
+                    if(bmp[0].size() == bmp[i].size() && bmp[0][0].size() == bmp[i][0].size())
+                    {
+                        i++;
+                        bmp.resize(i + 1);
+                    }
+                    else
+                    {
+                        cout << "Wrong dimensions" << endl;
+        
+                    }
+                }
+            }
+            else if(i != MaxImages)
+            {
+                cout << "File is not valid.\n";
+            }
+        
     }
 
     OverlayImages();
 
-    cout << "What name would you like to save this image with.\n";
+    cout << "What name would you like to save this image with." << endl;
     cin >> imgName;
 
     image.save(imgName + ".bmp");
-
-}
-
-
-bool goodImage(string imageString)
-{
-
-
 
 }
 
@@ -103,12 +94,14 @@ void OverlayImages()
     int RedSum = 0;
     int GreenSum = 0;
     int BlueSum = 0;
+    int progress = 0;
+    int quarterSize = bmp[0].size() / 4;
 
     for(int r = 0; r < bmp[0].size(); r++)
     {
         for(int c = 0; c < bmp[0][0].size(); c++)
         {
-            for(int i = 0; i < bmp.size(); i++)
+            for(int i = 0; i < bmp.size() - 1; i++)
             {
                 
               rgb = bmp[i][r][c];
@@ -123,6 +116,7 @@ void OverlayImages()
             rgb.green = GreenSum / bmp.size();
             rgb.blue = BlueSum / bmp.size();
             
+            
             RedSum = 0;
             GreenSum = 0;
             BlueSum = 0;
@@ -130,8 +124,17 @@ void OverlayImages()
             bmp[0][r][c] = rgb;
            
         }
-    }
 
+        if(r % quarterSize == 0 && progress != 3)
+        {
+    
+            progress++;
+            cout << "Image composition is " << progress << " quarters complete." << endl;
+
+        }
+    }
+    
     image.fromPixelMatrix(bmp[0]);
+     
 }
 
